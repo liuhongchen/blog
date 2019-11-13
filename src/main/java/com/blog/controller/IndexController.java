702513645e8644306d1d114f142966424ec02552
Vicent <halo_0@icloud.com>
2019-11-13 17:18:45 +0800

    back_1

--- src/main/java/com/blog/controller/IndexController.java
+++ src/main/java/com/blog/controller/IndexController.java
@@ -4,21 +4,41 @@ package com.blog.controller;
 import com.blog.exception.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name) throws NotFoundException {
-//        int a=1/0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
-        System.out.println("--------index---------");
-
+    @GetMapping("/")
+    public String index() throws NotFoundException {
         return "index";
     }
+
+    @GetMapping("/blog")
+    public String blog() throws NotFoundException {
+        return "blog";
+    }
+
+
+    @GetMapping("/tags")
+    public String tags() throws NotFoundException {
+        return "tags";
+    }
+
+    @GetMapping("/archives")
+    public String archives() throws NotFoundException {
+        return "archives";
+    }
+
+    @GetMapping("/types")
+    public String types() throws NotFoundException {
+        return "types";
+    }
+
+    @GetMapping("/about")
+    public String about() throws NotFoundException {
+        return "about";
+    }
+
+
 }
